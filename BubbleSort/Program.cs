02ab885fb7a9cfa844c637f6edb36d4f91c38287
Jerson Brito <jersonlbrito@hotmail.com>
2021-12-25 23:12:42 -0300

    init

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+
+var size = 100_000;
+
+var random = new Random();
+var array = new int[size];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array.SetValue(random.Next(0, 10), i);
+}
+
+ulong iterations = 0;
+var stopWatch = new Stopwatch();
+stopWatch.Start();
+for (int partIndex = array.Length - 1; partIndex > 0; partIndex--)
+{
+    for (int i = 0; i < partIndex; i++)
+    {
+        var j = i + 1;
+        iterations++;
+        if (array[i] > array[j])
+        {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+    }
+}
+stopWatch.Stop();
+
+Console.WriteLine($"{iterations} iterations: { stopWatch.ElapsedMilliseconds}ms");
---
+++ SelectionSort/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+
+var size = 100_000;
+
+var random = new Random();
+var array = new int[size];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array.SetValue(random.Next(0, size), i);
+}
+
+ulong iterations = 0;
+var stopWatch = new Stopwatch();
+stopWatch.Start();
+
+for (int partIndex = array.Length - 1; partIndex > 0; partIndex--)
+{
+    int largestAt = 0;
+
+    for (int i = 1; i <= partIndex; i++)
+    {
+        iterations++;
+
+        if (array[i] > array[largestAt])
+            largestAt = i;
+
+        int temp = array[largestAt];
+        array[largestAt] = array[partIndex];
+        array[partIndex] = temp;
+    }
+}
+stopWatch.Stop();
+
+Console.WriteLine($"{iterations} iterations: { stopWatch.ElapsedMilliseconds}ms");
