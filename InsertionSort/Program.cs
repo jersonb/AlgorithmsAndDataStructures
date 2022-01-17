f0165ad03f4dbd52c9afd2705b188eb48dc97090
jersonb <jersonlbrito@hotmail.com>
2022-01-16 21:22:55 -0300

    adding recursion

--- InsertionSort/Program.cs
+++ InsertionSort/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+var size = 20;
+
+var random = new Random();
+var array = new int[size];
+
+for (int i = 0; i < array.Length; i++)
+{
+    array.SetValue(random.Next(0, size), i);
+}
+
+ulong iterations = 0;
+var stopWatch = new Stopwatch();
+stopWatch.Start();
+
+for (int partIndex = 1; partIndex < array.Length; partIndex++)
+{
+    var curUnsorted = array[partIndex];
+    var i = 0;
+    for (i = partIndex; i > 0 && array[i - 1] > curUnsorted; i--)
+    {
+
+        iterations++;
+        array[i] = array[i - 1];
+    }
+    array[i] = curUnsorted;
+
+}
+stopWatch.Stop();
+
+Console.WriteLine($"{iterations} iterations: { stopWatch.ElapsedMilliseconds}ms");
---
+++ Recursion/Program.cs
@@ -0,0 +1,10 @@
+﻿
+static int Factorial(int n)
+{
+    if (n == 0)
+        return 1;
+
+    return n * Factorial(n - 1);
+}
+
+Console.WriteLine(Factorial(3));
